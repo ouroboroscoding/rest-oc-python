3ea629a42848cfaa4f79a26ff13b68dfa75a7334
Chris Nasr <chris@ouroboroscoding.com>
2023-03-14 09:33:53 -0400

    Release - Updated version - Updated arrow to 1.2.3 - Updated bottle to 0.12.25 - Updated Pillow to 9.4.0 - Updated redis to 4.5.1 - Updated requests to 2.28.2 - Removed hiredis - Added REST_LIST_TO_LONG error code - Added a way to add GET routes that handle multiple service requests and return them in the same order requested

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as oF:
 
 setup(
 	name='Rest-OC',
-	version='1.1.1',
+	version='1.2.0',
 	description='Rest-OC is a library of python 3 modules for rapidly setting up REST microservices.',
 	long_description=long_description,
 	long_description_content_type='text/markdown',
@@ -22,19 +22,18 @@ setup(
 	packages=['RestOC'],
 	python_requires='>=3.10',
 	install_requires=[
-		'arrow==1.2.2',
-		'bottle==0.12.23',
+		'arrow==1.2.3',
+		'bottle==0.12.25',
 		'format-oc==1.6.1',
 		'gunicorn==20.1.0',
-		'hiredis==2.0.0',
 		'Jinja2==3.1.2',
 		'markupsafe==2.0.1',
 		'pdfkit==1.0.0',
 		'piexif==1.1.3',
-		'Pillow==9.2.0',
+		'Pillow==9.4.0',
 		'PyMySQL==1.0.2',
-		'redis==4.3.4',
-		'requests==2.28.1',
+		'redis==4.5.1',
+		'requests==2.28.2',
 		'rethinkdb==2.4.9'
 	],
 	zip_safe=True
